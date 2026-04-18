fbd84a92c76676c3d54270a850ef35f8a1c6308c
Mostafa ElSheikh <melsheikh@mrsool.co>
2026-04-18 22:58:23 +0200

    init commit

---
+++ src/main/java/com/petroapp/stationtransfer/StationTransferApplication.java
@@ -0,0 +1,13 @@
+package com.petroapp.stationtransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StationTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StationTransferApplication.class, args);
+	}
+
+}
